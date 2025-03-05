b42034ce9b3b02cb207dcd0632b0f0be0c97c6a7
Andy Karpov <andykarpov@users.noreply.github.com>
2025-03-05 22:10:20 +0100

    Added v4

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 openscad-nightly -o release/v3_lid.stl -p v3.json -P "Print Lid" v3.scad
 openscad-nightly -o release/v3_base.stl -p v3.json -P "Print Base" v3.scad
 
+openscad-nightly -o release/v4_lid.stl -p v4.json -P "Print Lid" v4.scad
+openscad-nightly -o release/v4_base.stl -p v4.json -P "Print Base" v4.scad
